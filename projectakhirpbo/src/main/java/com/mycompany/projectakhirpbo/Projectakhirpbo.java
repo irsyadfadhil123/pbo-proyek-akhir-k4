c66731e7c7ea9999a74b68262b7cdc68ae6528b8
Irsyad Fadhil <71073281+irsyadfadhil123@users.noreply.github.com>
2024-05-12 20:53:22 +0700

    membuat project

---
+++ projectakhirpbo/src/main/java/com/mycompany/projectakhirpbo/Projectakhirpbo.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.projectakhirpbo;
+
+/**
+ *
+ * @author Irsyad Fadhil
+ */
+public class Projectakhirpbo {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        Database conn = new Database();
+        
+        conn.getConnection();
+    }
+}
