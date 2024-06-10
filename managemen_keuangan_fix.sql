d37c5cc48cb95da701fab2400e01cc5a0690d94b
mars050504 <153522723+mars050504@users.noreply.github.com>
2024-06-10 11:00:20 +0700

    Update managemen_keuangan_fix.sql

--- managemen_keuangan_fix.sql
+++ managemen_keuangan_fix.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.2.1
+-- version 5.1.1
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jun 08, 2024 at 03:42 AM
--- Server version: 10.4.28-MariaDB
--- PHP Version: 8.2.4
+-- Waktu pembuatan: 10 Jun 2024 pada 05.59
+-- Versi server: 10.4.21-MariaDB
+-- Versi PHP: 7.3.31
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -24,7 +24,7 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Table structure for table `laporankeuangan`
+-- Struktur dari tabel `laporankeuangan`
 --
 
 CREATE TABLE `laporankeuangan` (
@@ -32,22 +32,22 @@ CREATE TABLE `laporankeuangan` (
   `tanggal` date DEFAULT NULL,
   `total_penjualan` decimal(15,2) DEFAULT NULL,
   `total_pembelian` decimal(15,2) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `pelanggan`
+-- Struktur dari tabel `pelanggan`
 --
 
 CREATE TABLE `pelanggan` (
   `id_pelanggan` int(11) NOT NULL,
   `nama_pelanggan` varchar(100) DEFAULT NULL,
   `kontak_pelanggan` varchar(100) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
--- Dumping data for table `pelanggan`
+-- Dumping data untuk tabel `pelanggan`
 --
 
 INSERT INTO `pelanggan` (`id_pelanggan`, `nama_pelanggan`, `kontak_pelanggan`) VALUES
@@ -56,7 +56,7 @@ INSERT INTO `pelanggan` (`id_pelanggan`, `nama_pelanggan`, `kontak_pelanggan`) V
 -- --------------------------------------------------------
 
 --
--- Table structure for table `pembelian`
+-- Struktur dari tabel `pembelian`
 --
 
 CREATE TABLE `pembelian` (
@@ -66,10 +66,10 @@ CREATE TABLE `pembelian` (
   `jumlah_barang` int(11) DEFAULT NULL,
   `uang_keluar` int(20) NOT NULL,
   `catatan` varchar(50) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
--- Dumping data for table `pembelian`
+-- Dumping data untuk tabel `pembelian`
 --
 
 INSERT INTO `pembelian` (`id_pembelian`, `tanggal`, `id_barang`, `jumlah_barang`, `uang_keluar`, `catatan`) VALUES
@@ -77,7 +77,7 @@ INSERT INTO `pembelian` (`id_pembelian`, `tanggal`, `id_barang`, `jumlah_barang`
 (2, '2024-05-30', 1, 10, 50000, 'ini tes');
 
 --
--- Triggers `pembelian`
+-- Trigger `pembelian`
 --
 DELIMITER $$
 CREATE TRIGGER `after_pembelian_delete` AFTER DELETE ON `pembelian` FOR EACH ROW BEGIN
@@ -110,27 +110,28 @@ DELIMITER ;
 -- --------------------------------------------------------
 
 --
--- Table structure for table `pemilikusaha`
+-- Struktur dari tabel `pemilikusaha`
 --
 
 CREATE TABLE `pemilikusaha` (
   `id_pemilik` int(11) NOT NULL,
-  `nama` varchar(100) DEFAULT NULL,
-  `username` varchar(100) DEFAULT NULL,
+  `full_name` varchar(100) DEFAULT NULL,
+  `email` varchar(100) DEFAULT NULL,
   `password` varchar(20) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
--- Dumping data for table `pemilikusaha`
+-- Dumping data untuk tabel `pemilikusaha`
 --
 
-INSERT INTO `pemilikusaha` (`id_pemilik`, `nama`, `username`, `password`) VALUES
-(1, 'Admin', 'admin', 'admin');
+INSERT INTO `pemilikusaha` (`id_pemilik`, `full_name`, `email`, `password`) VALUES
+(1, 'Admin', 'admin', 'admin'),
+(2, 'mario', 'mario@gmail.com', '123');
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `penjualan`
+-- Struktur dari tabel `penjualan`
 --
 
 CREATE TABLE `penjualan` (
@@ -140,10 +141,10 @@ CREATE TABLE `penjualan` (
   `jumlah_barang` int(11) DEFAULT NULL,
   `uang_masuk` int(20) NOT NULL,
   `catatan` varchar(50) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
--- Dumping data for table `penjualan`
+-- Dumping data untuk tabel `penjualan`
 --
 
 INSERT INTO `penjualan` (`id_penjualan`, `tanggal`, `id_barang`, `jumlah_barang`, `uang_masuk`, `catatan`) VALUES
@@ -153,7 +154,7 @@ INSERT INTO `penjualan` (`id_penjualan`, `tanggal`, `id_barang`, `jumlah_barang`
 (11, '2024-06-05', 3, 10, 30000, NULL);
 
 --
--- Triggers `penjualan`
+-- Trigger `penjualan`
 --
 DELIMITER $$
 CREATE TRIGGER `after_penjualan_delete` AFTER DELETE ON `penjualan` FOR EACH ROW BEGIN
@@ -186,7 +187,7 @@ DELIMITER ;
 -- --------------------------------------------------------
 
 --
--- Table structure for table `piutang`
+-- Struktur dari tabel `piutang`
 --
 
 CREATE TABLE `piutang` (
@@ -195,10 +196,10 @@ CREATE TABLE `piutang` (
   `tanggal` date NOT NULL,
   `jumlah` int(11) NOT NULL,
   `catatan` varchar(100) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
--- Dumping data for table `piutang`
+-- Dumping data untuk tabel `piutang`
 --
 
 INSERT INTO `piutang` (`id_piutang`, `id_pelanggan`, `tanggal`, `jumlah`, `catatan`) VALUES
@@ -207,7 +208,7 @@ INSERT INTO `piutang` (`id_piutang`, `id_pelanggan`, `tanggal`, `jumlah`, `catat
 -- --------------------------------------------------------
 
 --
--- Table structure for table `stokbarang`
+-- Struktur dari tabel `stokbarang`
 --
 
 CREATE TABLE `stokbarang` (
@@ -216,30 +217,30 @@ CREATE TABLE `stokbarang` (
   `jumlah` int(11) DEFAULT NULL,
   `harga_satuan` decimal(15,2) DEFAULT NULL,
   `peringatan_minimum` int(11) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
--- Dumping data for table `stokbarang`
+-- Dumping data untuk tabel `stokbarang`
 --
 
 INSERT INTO `stokbarang` (`id_barang`, `nama_barang`, `jumlah`, `harga_satuan`, `peringatan_minimum`) VALUES
-(1, 'snack', 100, 5000.00, 10),
-(3, 'Roti', 0, 3000.00, 5);
+(1, 'snack', 100, '5000.00', 10),
+(3, 'Roti', 0, '3000.00', 5);
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `supplier`
+-- Struktur dari tabel `supplier`
 --
 
 CREATE TABLE `supplier` (
   `id_supplier` int(11) NOT NULL,
   `nama_supplier` varchar(100) DEFAULT NULL,
   `kontak_supplier` varchar(100) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
--- Dumping data for table `supplier`
+-- Dumping data untuk tabel `supplier`
 --
 
 INSERT INTO `supplier` (`id_supplier`, `nama_supplier`, `kontak_supplier`) VALUES
@@ -248,19 +249,19 @@ INSERT INTO `supplier` (`id_supplier`, `nama_supplier`, `kontak_supplier`) VALUE
 -- --------------------------------------------------------
 
 --
--- Table structure for table `targetkeuangan`
+-- Struktur dari tabel `targetkeuangan`
 --
 
 CREATE TABLE `targetkeuangan` (
   `id_target` int(11) NOT NULL,
   `target_pendapatan` decimal(15,2) DEFAULT NULL,
   `periode` varchar(50) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `utang`
+-- Struktur dari tabel `utang`
 --
 
 CREATE TABLE `utang` (
@@ -269,10 +270,10 @@ CREATE TABLE `utang` (
   `tanggal` date DEFAULT NULL,
   `jumlah` int(11) NOT NULL,
   `catatan` varchar(150) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
--- Dumping data for table `utang`
+-- Dumping data untuk tabel `utang`
 --
 
 INSERT INTO `utang` (`id_utang`, `id_supplier`, `tanggal`, `jumlah`, `catatan`) VALUES
@@ -283,146 +284,146 @@ INSERT INTO `utang` (`id_utang`, `id_supplier`, `tanggal`, `jumlah`, `catatan`) 
 --
 
 --
--- Indexes for table `laporankeuangan`
+-- Indeks untuk tabel `laporankeuangan`
 --
 ALTER TABLE `laporankeuangan`
   ADD PRIMARY KEY (`id_laporan`);
 
 --
--- Indexes for table `pelanggan`
+-- Indeks untuk tabel `pelanggan`
 --
 ALTER TABLE `pelanggan`
   ADD PRIMARY KEY (`id_pelanggan`);
 
 --
--- Indexes for table `pembelian`
+-- Indeks untuk tabel `pembelian`
 --
 ALTER TABLE `pembelian`
   ADD PRIMARY KEY (`id_pembelian`),
   ADD KEY `id_barang` (`id_barang`);
 
 --
--- Indexes for table `pemilikusaha`
+-- Indeks untuk tabel `pemilikusaha`
 --
 ALTER TABLE `pemilikusaha`
   ADD PRIMARY KEY (`id_pemilik`);
 
 --
--- Indexes for table `penjualan`
+-- Indeks untuk tabel `penjualan`
 --
 ALTER TABLE `penjualan`
   ADD PRIMARY KEY (`id_penjualan`),
   ADD KEY `id_barang` (`id_barang`) USING BTREE;
 
 --
--- Indexes for table `piutang`
+-- Indeks untuk tabel `piutang`
 --
 ALTER TABLE `piutang`
   ADD PRIMARY KEY (`id_piutang`),
   ADD KEY `id_pelanggan` (`id_pelanggan`);
 
 --
--- Indexes for table `stokbarang`
+-- Indeks untuk tabel `stokbarang`
 --
 ALTER TABLE `stokbarang`
   ADD PRIMARY KEY (`id_barang`),
   ADD UNIQUE KEY `nama_barang` (`nama_barang`);
 
 --
--- Indexes for table `supplier`
+-- Indeks untuk tabel `supplier`
 --
 ALTER TABLE `supplier`
   ADD PRIMARY KEY (`id_supplier`);
 
 --
--- Indexes for table `targetkeuangan`
+-- Indeks untuk tabel `targetkeuangan`
 --
 ALTER TABLE `targetkeuangan`
   ADD PRIMARY KEY (`id_target`);
 
 --
--- Indexes for table `utang`
+-- Indeks untuk tabel `utang`
 --
 ALTER TABLE `utang`
   ADD PRIMARY KEY (`id_utang`),
   ADD KEY `id_supplier` (`id_supplier`);
 
 --
--- AUTO_INCREMENT for dumped tables
+-- AUTO_INCREMENT untuk tabel yang dibuang
 --
 
 --
--- AUTO_INCREMENT for table `laporankeuangan`
+-- AUTO_INCREMENT untuk tabel `laporankeuangan`
 --
 ALTER TABLE `laporankeuangan`
   MODIFY `id_laporan` int(11) NOT NULL AUTO_INCREMENT;
 
 --
--- AUTO_INCREMENT for table `pelanggan`
+-- AUTO_INCREMENT untuk tabel `pelanggan`
 --
 ALTER TABLE `pelanggan`
   MODIFY `id_pelanggan` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
--- AUTO_INCREMENT for table `pembelian`
+-- AUTO_INCREMENT untuk tabel `pembelian`
 --
 ALTER TABLE `pembelian`
   MODIFY `id_pembelian` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
--- AUTO_INCREMENT for table `pemilikusaha`
+-- AUTO_INCREMENT untuk tabel `pemilikusaha`
 --
 ALTER TABLE `pemilikusaha`
-  MODIFY `id_pemilik` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id_pemilik` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
--- AUTO_INCREMENT for table `penjualan`
+-- AUTO_INCREMENT untuk tabel `penjualan`
 --
 ALTER TABLE `penjualan`
   MODIFY `id_penjualan` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
 
 --
--- AUTO_INCREMENT for table `piutang`
+-- AUTO_INCREMENT untuk tabel `piutang`
 --
 ALTER TABLE `piutang`
   MODIFY `id_piutang` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
--- AUTO_INCREMENT for table `stokbarang`
+-- AUTO_INCREMENT untuk tabel `stokbarang`
 --
 ALTER TABLE `stokbarang`
   MODIFY `id_barang` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
--- AUTO_INCREMENT for table `supplier`
+-- AUTO_INCREMENT untuk tabel `supplier`
 --
 ALTER TABLE `supplier`
   MODIFY `id_supplier` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
--- AUTO_INCREMENT for table `targetkeuangan`
+-- AUTO_INCREMENT untuk tabel `targetkeuangan`
 --
 ALTER TABLE `targetkeuangan`
   MODIFY `id_target` int(11) NOT NULL AUTO_INCREMENT;
 
 --
--- AUTO_INCREMENT for table `utang`
+-- AUTO_INCREMENT untuk tabel `utang`
 --
 ALTER TABLE `utang`
   MODIFY `id_utang` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
--- Constraints for dumped tables
+-- Ketidakleluasaan untuk tabel pelimpahan (Dumped Tables)
 --
 
 --
--- Constraints for table `piutang`
+-- Ketidakleluasaan untuk tabel `piutang`
 --
 ALTER TABLE `piutang`
   ADD CONSTRAINT `piutang_ibfk_1` FOREIGN KEY (`id_pelanggan`) REFERENCES `pelanggan` (`id_pelanggan`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
--- Constraints for table `utang`
+-- Ketidakleluasaan untuk tabel `utang`
 --
 ALTER TABLE `utang`
   ADD CONSTRAINT `utang_ibfk_1` FOREIGN KEY (`id_supplier`) REFERENCES `supplier` (`id_supplier`) ON DELETE CASCADE ON UPDATE CASCADE;
